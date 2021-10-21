9b808d254e9cb10b8f78815fccb3fe3cd3e1d6e8
LDA <lda@ldasuku.net>
2021-10-20 18:21:08 -0700

    add a pause menu and capture the mouse cursor

---
+++ source/PauseMenu/PauseMenu.gd
@@ -0,0 +1,29 @@
+class_name PauseMenu
+extends Control
+
+func pause():
+	get_tree().paused = true
+	self.show()
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func unpause():
+	get_tree().paused = false
+	self.hide()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _ready():
+	self.hide()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _on_ReturnButton_pressed():
+	unpause()
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
+
+func _unhandled_key_input(event):
+	if event.pressed and event.scancode == KEY_ESCAPE:
+		if get_tree().paused:
+			unpause()
+		else:
+			pause()
