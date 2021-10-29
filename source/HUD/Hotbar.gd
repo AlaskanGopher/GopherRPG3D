a95fc1117afe53a278967de6409da94fda68225f
BoppleOpple <boppleopple@gmail.com>
2021-10-28 21:05:12 -0400

    added hotbar switching w/ number keys

--- source/HUD/Hotbar.gd
+++ source/HUD/Hotbar.gd
@@ -2,6 +2,8 @@ extends HBoxContainer
 
 export var SLOTS : int = 6
 
+var selectedSlot = 0
+
 func _ready():
 	rect_size.x = SLOTS*64
 	rect_position.x = get_parent().rect_size.x - rect_size.x
@@ -15,7 +17,21 @@ func _ready():
 		if i == 0:
 			slot.region_rect = Rect2(0, 0, 32, 32)
 		elif i == SLOTS-1:
-			slot.region_rect = Rect2(32, 0, 32, 32)
-		else:
 			slot.region_rect = Rect2(64, 0, 32, 32)
+		else:
+			slot.region_rect = Rect2(32, 0, 32, 32)
 		add_child(slot)
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				print(get_global_mouse_position())
+			elif event.button_index == BUTTON_WHEEL_DOWN:
+				pass
+	if event is InputEventKey:
+		if event.scancode >= KEY_0 and event.scancode <= KEY_9 and event.pressed:
+			selectedSlot = event.scancode - KEY_1
+			selectedSlot = 9 if selectedSlot == -1 else selectedSlot
+			get_node("../SelectedSlot").rect_position.x = get_global_rect().position.x + selectedSlot * 64
+			print(get_global_rect().position.x +selectedSlot * 64)
