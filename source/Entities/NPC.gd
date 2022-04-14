b1393c4845c980cc0cdf7d888aef9df14860c7f4
Kirite-Kamito <jackwolfpack4@gmail.com>
2022-04-14 13:38:04 -0400

    Revert "Parsing json for dialog function"
    This reverts commit 75a313c0c83f7159ba2d3595d6250f6ebeae91f7.

--- source/Entities/NPC.gd
+++ source/Entities/NPC.gd
@@ -5,7 +5,6 @@ export var MAX_HEALTH : int = 100
 var velocity : Vector3 = Vector3()
 var airTime = 0;
 var hp: int = MAX_HEALTH
-var id = "1"
 
 func _physics_process(delta):
 	var acceleration = Vector3()
@@ -29,18 +28,8 @@ func onHealthChange(var difference: int):
 func playerInteraction(var player : KinematicBody):
 	if Input.is_action_just_pressed("object_interact"):
 		player.get_node("./HUD/ButtonPrompt").visible = false
-		# Read dialogue from file
 		var file = File.new()
-		file.open("res://Resources/Dialogue/dialogueStory.json", File.READ)
-		var resultJson = JSON.parse(file.get_as_text())
-		file.close()
-		if resultJson.error != OK:
-			print("Error: ", resultJson.error)
-			print("Error Line: ", resultJson.error_line)
-			print("Error String: ", resultJson.error_string)
-		var dialogueInfo = resultJson.result[id]
-		print(dialogueInfo["name"])
-		print(dialogueInfo["color"])
-		print(dialogueInfo["text"])
+		file.open("res://Resources/Dialogue/testJson.json", File.READ)
+		print(parse_json(file.get_as_text())["text"])
 	else:
 		player.get_node("./HUD").prompt(BUTTON_RIGHT)
